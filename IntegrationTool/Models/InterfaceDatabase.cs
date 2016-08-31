3642c35c6b9f2731b045817bc022da3236dc4e17
cturcios <cristian.turcios@laureate.net>
2016-08-31 10:03:55 -0600

    Integration Tool with interface Database

---
+++ IntegrationTool/Models/InterfaceDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Models
+{
+    interface InterfaceDatabase
+    {
+        string connectionStringDatabase { get; set; }
+        string ip { get; set; }
+        string name { get; set; }
+        string instance { get; set; }
+        string username { get; set; }
+        string password { get; set; }
+
+        void openConexion();
+        void closeConexion();
+        string executeQuery(string query);
+    }
+}
