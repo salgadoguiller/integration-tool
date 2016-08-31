d15b8fba7ec9ca3059a31f52816e5d8a073d7ad5
cturcios <cristian.turcios@laureate.net>
2016-08-31 14:36:35 -0600

    Class Mysql Database

---
+++ IntegrationTool.Tests/TestMysqlDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntegrationTool.Models;
+using NUnit.Framework;
+
+namespace IntegrationTool.Tests
+{
+    [TestFixture]
+    class TestMysqlDatabase
+    {
+        [Test]
+        public void pruebaDeConexion()
+        {
+            MySqlDatabase patito = new MySqlDatabase("","prueba", "localhost", "root", "","3306");
+            string gg =patito.executeQuery("select * from prueba2");           
+        }
+    }
+}
--- IntegrationTool/Models/InterfaceDatabase.cs
+++ IntegrationTool/Models/InterfaceDatabase.cs
@@ -7,16 +7,9 @@ using System.Threading.Tasks;
 namespace IntegrationTool.Models
 {
     interface InterfaceDatabase
-    {
-        string connectionStringDatabase { get; set; }
-        string ip { get; set; }
-        string name { get; set; }
-        string instance { get; set; }
-        string username { get; set; }
-        string password { get; set; }
-
-        void openConexion();
-        void closeConexion();
+    {      
+        void openConnection();
+        void closeConnection();
         string executeQuery(string query);
     }
 }
