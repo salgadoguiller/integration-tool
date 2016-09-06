2827f76d02b5d758a39b888937feba6e6777dcd5
Guillermo Crescencio Salgado <guillermo.crescencio@laureate.net>
2016-09-06 15:11:49 -0600

    Validated views

---
+++ IntegrationTool/Content/Scripts/Directives/FeedbackMessages.js
@@ -0,0 +1,13 @@
+﻿var feedBackMessagesDirective = function () {
+    return {
+        restrict: 'E',
+        scope: {
+            type: '=',
+            message: '='
+        },
+        template:   '<div ng-hide="type === 0" class="alert alert-{{type}} alert-dismissible col-md-10 col-md-offset-1" role="alert">' +
+                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+                        '<span class="fa fa-check-circle" aria-hidden="true"></span><strong></strong> {{message}}' +
+                    '</div>'
+    }
+}
