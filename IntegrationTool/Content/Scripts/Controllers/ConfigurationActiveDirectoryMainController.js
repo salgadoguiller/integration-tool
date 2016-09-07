10a4d922c77781dd0dbf615f40ecd642c776977b
cturcios <cristian.turcios@laureate.net>
2016-09-07 15:06:39 -0600

    Table Responsive

--- IntegrationTool/Content/Scripts/Controllers/ConfigurationActiveDirectoryMainController.js
+++ IntegrationTool/Content/Scripts/Controllers/ConfigurationActiveDirectoryMainController.js
@@ -1,6 +1,9 @@
 ﻿var ConfigurationActiveDirectoryMainController = function($scope, $http) {
-   
-    $("#example1").DataTable();
+
+    $("#example1")
+        .DataTable({
+            responsive: false
+        });
 
 }
 ConfigurationActiveDirectoryMainController.$inject = ['$scope', '$http'];
