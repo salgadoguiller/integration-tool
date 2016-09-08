3da2c0a0ccfc4ea664ba85710a0fb6d71aeefe36
Guillermo Crescencio Salgado <guillermo.crescencio@laureate.net>
2016-09-08 10:29:43 -0600

    Fixing the previous error commit

--- IntegrationTool/Content/Scripts/Controllers/ConfigurationActiveDirectoryMainController.js
+++ IntegrationTool/Content/Scripts/Controllers/ConfigurationActiveDirectoryMainController.js
@@ -1,6 +1,11 @@
 ﻿var ConfigurationActiveDirectoryMainController = function($scope, $http) {
    
-    $("#example1").DataTable();
+    $("#example1").DataTable({
+        "columnDefs": [
+            { responsivePriority: 1, targets: 0 },
+            { responsivePriority: 2, targets: -1 }
+        ]
+    });
 
 }
 ConfigurationActiveDirectoryMainController.$inject = ['$scope', '$http'];
