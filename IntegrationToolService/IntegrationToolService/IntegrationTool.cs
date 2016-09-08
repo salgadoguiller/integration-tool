648286eb586694f2cf5f12a7d97e54267f805296
cturcios <cristian.turcios@laureate.net>
2016-09-08 11:45:06 -0600

    Add Class Library, Add Integration Tool Service

---
+++ IntegrationToolService/IntegrationToolService/IntegrationTool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace IntegrationToolService
+{
+    public partial class IntegrationTool : ServiceBase
+    {
+        public IntegrationTool()
+        {
+            InitializeComponent();
+          
+        }
+      
+        protected override void OnStart(string[] args)
+        {
+            var timer = new Timer { AutoReset = true, Interval = 60000 };
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            WriteFile writeFile = new WriteFile();
+            writeFile.writeFileByString2("ğçşüexternal_course_key|course_id|course_name|term_keyñá|duration|master_course_key|new_data_source_key", "CHE101002|CHE101002|General Chemistry 2 002|201206|t|CHE101M|201206_COURSES", @"C:\Users\cturcios\Desktop", "COURSES");
+            writeFile.writeIntegrationinExcel("iÝğçşüexternal_course_key|course_id|course_name|term_key|duration|master_course_key|new_data_source_key", "CHE101002|CHE101002|General Chemistry 2 002|201206|t|CHE101M|201206_COURSES", @"C:\Users\cturcios\Desktop", "COURSES");
+           /* var processes = Process.GetProcessesByName("firefox");
+
+            foreach (var process in processes)
+            {
+                process.CloseMainWindow();
+
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    process.Close();
+                }
+            }*/
+        }
+    }
+}
---
+++ IntegrationToolService/IntegrationToolService/WriteFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationToolService
+{
+    class WriteFile
+    {
+        public void writeFileByString2(string headers, string resultQuery, string locationToSave, string nameIntegration)
+        {
+            string path = locationToSave + @"\" + nameIntegration + "-" + returnDatetimeNow() + ".txt";
+            FileStream fs = new FileStream(path, FileMode.Append);
+            using (StreamWriter file = new StreamWriter(fs, Encoding.UTF8))
+            {
+                file.Write(headers);
+                file.WriteLine();
+                file.Write(resultQuery);
+            }
+        }
+
+        public void writeIntegrationinExcel(string headers, string resultQuery, string locationToSave,
+            string nameIntegration)
+        {
+            StringBuilder csvContent = new StringBuilder();
+            string header = headers.Replace("|", ",");
+            string query = resultQuery.Replace("|", ",");
+
+            csvContent.AppendLine(header);
+            csvContent.AppendLine(query);
+            string path = locationToSave + @"\" + nameIntegration + returnDatetimeNow() + ".csv";
+            File.AppendAllText(path, csvContent.ToString());
+        }
+
+        public string returnDatetimeNow()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
