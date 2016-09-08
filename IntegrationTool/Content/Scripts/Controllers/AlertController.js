d24d66ca88b3267757d7385da38ebe2e3c7d2281
Guillermo Crescencio Salgado <guillermo.crescencio@laureate.net>
2016-09-08 09:00:21 -0600

    Add AlertController

---
+++ IntegrationTool/Content/Scripts/Controllers/AlertController.js
@@ -0,0 +1,16 @@
+﻿var AlertController = function ($scope, $http) {
+    $scope.sweet = {};
+    $scope.sweet.option = {
+        title: "Are you sure?",
+        text: "Press confirm to continue.",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#0e5555",
+        confirmButtonText: "Confirm",
+        cancelButtonText: "Cancel",
+        closeOnConfirm: true,
+        closeOnCancel: true
+    }
+}
+
+AlertController.$inject = ['$scope', '$http'];
