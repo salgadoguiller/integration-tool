9078634ee71d8068d66da5cf314b734d96a53a46
Guillermo Crescencio Salgado <guillermo.crescencio@laureate.net>
2016-09-09 14:27:58 -0600

    Update for configuration parameters.

---
+++ IntegrationTool/IntegrationTool/Content/Scripts/Controllers/FormDataBaseController.js
@@ -0,0 +1,111 @@
+﻿var FormDataBaseController = function ($scope, $http, $routeParams) {
+    $scope.typeMessage = 0;
+    $scope.message = "";
+    $scope.request = {};
+    $scope.regexIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    $scope.regexPort = /^([0-9]*)$/;
+    $scope.sendRequest = sendRequest;
+    $scope.dataBaseEngines = [];
+
+    getDataBaseEngines();
+
+    loadInfo();
+
+    function loadInfo() {
+        if ($routeParams.id == -1) {
+            return;
+        }
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        var data = $.param({});
+
+        $http.get('Configuration/getDataBase?id=' + $routeParams.id, data, config).success(function (resp) {
+            if (resp.type !== 'danger') {
+                $scope.request = resp;
+            } else {
+                $scope.message = resp.message;
+                $scope.typeMessage = resp.type;
+            }
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+
+    function getDataBaseEngines() {
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        var data = $.param({});
+
+        $http.get('Configuration/getDataBaseEngines', data, config).success(function (resp) {
+            if (resp.type !== 'danger') {
+                $scope.dataBaseEngines = resp;
+            }else {
+                $scope.message = resp.message;
+                $scope.typeMessage = resp.type;
+            }
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+
+    function sendRequest(req, form) {
+        if (!form.$valid) {
+            $scope.message = "Error: Invalid form, please try again.";
+            $scope.typeMessage = "danger";
+            return false;
+        }
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        var data = $.param(req);
+
+        if ($routeParams.id == -1)
+            put(data, config, form);
+        else
+            post(data, config, form);
+    }
+
+    function put(data, config, form) {
+        $http.put('Configuration/saveDataBase', data, config).success(function (resp) {
+            $scope.message = resp.message;
+            $scope.typeMessage = resp.type;
+            $scope.request = {};
+            form.$setPristine();
+            form.$setUntouched();
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+
+    function post(data, config, form) {
+        $http.post('Configuration/updateDataBase', data, config).success(function (resp) {
+            $scope.message = resp.message;
+            $scope.typeMessage = resp.type;
+            $scope.request = {};
+            loadInfo();
+            form.$setPristine();
+            form.$setUntouched();
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+}
+
+FormDataBaseController.$inject = ['$scope', '$http', '$routeParams'];
---
+++ IntegrationTool/IntegrationTool/Content/Scripts/Controllers/FormWebServiceController.js
@@ -0,0 +1,84 @@
+﻿var FormWebServiceController = function ($scope, $http, $routeParams) {
+    $scope.typeMessage = 0;
+    $scope.message = "";
+    $scope.request = {};
+    $scope.sendRequest = sendRequest;
+
+    loadInfo();
+
+    function loadInfo() {
+        if ($routeParams.id == -1) {
+            return;
+        }
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        var data = $.param({});
+
+        $http.get('Configuration/getWebService?id=' + $routeParams.id, data, config).success(function (resp) {
+            if (resp.type !== 'danger') {
+                $scope.request = resp;
+            } else {
+                $scope.message = resp.message;
+                $scope.typeMessage = resp.type;
+            }
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+
+    function sendRequest(req, form) {
+        if (!form.$valid) {
+            $scope.message = "Error: Invalid form, please try again.";
+            $scope.typeMessage = "danger";
+            return false;
+        }
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        var data = $.param(req);
+
+        if ($routeParams.id == -1)
+            put(data, config, form);
+        else
+            post(data, config, form);
+    }
+
+    function put(data, config, form) {
+        $http.put('Configuration/saveWebService', data, config).success(function (resp) {
+            $scope.message = resp.message;
+            $scope.typeMessage = resp.type;
+            $scope.request = {};
+            form.$setPristine();
+            form.$setUntouched();
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+
+    function post(data, config, form) {
+        $http.post('Configuration/updateWebService', data, config).success(function (resp) {
+            $scope.message = resp.message;
+            $scope.typeMessage = resp.type;
+            $scope.request = {};
+            loadInfo();
+            form.$setPristine();
+            form.$setUntouched();
+        }).error(function (resp) {
+            $scope.message = "Error: " + resp;
+            $scope.typeMessage = "danger";
+        });
+    }
+}
+
+FormWebServiceController.$inject = ['$scope', '$http', '$routeParams'];
